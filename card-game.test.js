b240c638ea62a1f22734d1b50fec6e6f3922be96
Anna Shatilova <anxet@mail.ru>
2023-08-04 17:26:34 +0300

    Команда test не работает

--- card-game.test.js
+++ card-game.test.js
@@ -1,1 +1,10 @@
-// const { it } = require('@jest/globals')
+// const { it, expect } = require('@jest/globals')
+// import { getCardSuit } from 'index'
+
+// describe('cardGame', () => {
+//     it('should add suit to card', () => {
+//         const suitCard = new getCardSuit(3)
+
+//         expect(suitCard).toBe(suit)
+//     })
+// })
