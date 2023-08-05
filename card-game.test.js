ad9d67e8742a0548c33ba736e85e01052a10c682
Anna Shatilova <anxet@mail.ru>
2023-08-05 15:52:51 +0300

    Добавлен конфиг файл для jest

--- card-game.test.js
+++ card-game.test.js
@@ -1,10 +1,8 @@
-// const { it, expect } = require('@jest/globals')
-// import { getCardSuit } from 'index'
+const { it, expect, describe } = require('@jest/globals')
+import { getCardSuit } from './index'
 
-// describe('cardGame', () => {
-//     it('should add suit to card', () => {
-//         const suitCard = new getCardSuit(3)
-
-//         expect(suitCard).toBe(suit)
-//     })
-// })
+describe('cardGame', () => {
+    it('should add suit to card', () => {
+        expect(getCardSuit(3)).toBe('spades')
+    })
+})
