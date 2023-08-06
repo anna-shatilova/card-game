d0810f3a93520500d3d3c2479b07063939d8b03e
Anna Shatilova <anxet@mail.ru>
2023-08-06 19:37:21 +0300

    Смена расширения тестового файла, попытка добавить доп настройки в файл конфига. Не работает jest

--- card-game.test.js
+++
@@ -1,8 +0,0 @@
-const { it, expect, describe } = require('@jest/globals')
-import { getCardSuit } from './index'
-
-describe('cardGame', () => {
-    it('should add suit to card', () => {
-        expect(getCardSuit(3)).toBe('spades')
-    })
-})
---
+++ card-game.test.ts
@@ -0,0 +1,26 @@
+import { it, expect, describe } from '@jest/globals'
+import { getCardSuit, getShufflePairs } from './index'
+
+describe('cardGame', () => {
+    it('should add suit to card', () => {
+        expect(getCardSuit(3)).toBe('spades')
+    })
+
+    it('should create different arrays', () => {
+        const count = 3
+
+        const array1 = getShufflePairs(count)
+        const array2 = getShufflePairs(count)
+
+        expect(array1).not.toEqual(array2)
+    })
+
+    it('should return the correct number of cards on the table ', () => {
+        const count = 6
+
+        const cardsOnTable = getShufflePairs(count)
+        const expectedArrayLengthCards = count * 2
+
+        expect(cardsOnTable).toHaveLength(expectedArrayLengthCards)
+    })
+})
